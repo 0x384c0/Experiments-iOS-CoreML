f4eb6a954293c156036a38aed2f6f8930743139e
0x384c0 <0x384c0@gmail.com>
2018-10-25 17:22:02 +0600

    Initial Commit

---
+++ Experiments-CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Experiments-CoreML
+//
+//  Created by 0x384c0 on 10/25/18.
+//  Copyright © 2018 0x384c0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
